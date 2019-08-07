73d8e869796d8659ea37a851b3acf4a3ca095bc3
Steve Rumizen <steverumizen@gmail.com>
2019-08-07 15:11:51 -0600

    Add styling to sections functionality

--- src/containers/PerformPage/PerformPage.js
+++ src/containers/PerformPage/PerformPage.js
@@ -20,31 +20,37 @@ export class PerformPage extends Component {
   renderAllBars = () => {
     const activeLyric = this.props.lyrics.find(lyric => lyric.active);
 
-    if (activeLyric) {
-      return activeLyric.bars.map((bar, index) => {
-        if (bar.text.length) {
-          return (
-            <div key={bar.id} className="display-bar">
-              <p className="display-bar-num">{index + 1}</p>
-              <p className="display-bar-text">{bar.text}</p>
-            </div>
-          );
-        }
-      });
-    }
+    return activeLyric.sections.map(section => {
+      return (
+        <article className="lyric-section">
+          <h3>{section.title}</h3>
+          {section.bars.map((bar, index) => {
+            if (bar.text.length) {
+              return (
+                <div key={bar.id} className="display-bar">
+                  <p className="display-bar-num">{index + 1}</p>
+                  <p className="display-bar-text">{bar.text}</p>
+                </div>
+              );
+            }
+          })}
+        </article>
+      );
+    });
   };
 
   render() {
     const activeLyric = this.props.lyrics.find(lyric => lyric.active);
     return (
-      
       <main className="perform-page">
         <img
           src="./images/performPageImg.jpg"
           alt="rapper on stage performing with a mic on a blue and purple lit stage"
         />
         <section className="perform-page-bars-wrapper">
-          <h2 className="perform-lyric-title">{activeLyric && activeLyric.title}</h2>
+          <h2 className="perform-lyric-title">
+            {activeLyric && activeLyric.title}
+          </h2>
           <div className={`scroll-section ${this.state.scroll && "scroll-on"}`}>
             {activeLyric && this.renderAllBars()}
           </div>
